108297bc154d411313e8be8bde8671ceeb06835d
priyamishra2903 <63705581+priyamishra2903@users.noreply.github.com>
2020-09-10 19:58:09 +0530

    update

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body> 
-
 <form action="level.php" method="post">
   <h2>PLAYER NAME</h2>
   <input type="text" name="name" placeholder="NAME" autocomplete="false">
