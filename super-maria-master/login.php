d5202ec0531ca58a5014e881fbc7cce94b5b498b
priyamishra2903 <63705581+priyamishra2903@users.noreply.github.com>
2020-09-10 12:29:41 +0530

    Add files via upload
    php file

---
+++ super-maria-master/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Grab the coin</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="mystyle.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body> 
+
+<form action="level.php" method="post">
+  <h2>PLAYER NAME</h2>
+  <input type="text" name="name" placeholder="NAME" autocomplete="false">
+    
+  <input type="submit" value="Submit">
+    </form>
+
+
+</body>
+</html>
